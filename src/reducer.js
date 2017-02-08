a5354e341a7b3ef8132f98ad3326466bd741cfbf
Richard Torruellas <richard.torruellas@am.sony.com>
2017-02-08 15:46:08 -0800

    update deps

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,6 @@ import Immutable from 'immutable';
 const immutableInitialState = Immutable.Map();
 
 export function gclient(state = immutableInitialState, action) {
-  console.log(action);
   switch (action.type) {
     case '@@GQ_CLIENT/QUERY/SUCCESS':
     case '@@GQ_CLIENT/QUERY/ERROR':

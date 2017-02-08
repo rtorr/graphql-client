035b9759e08fa62dce1be1b17779e38281d1b8ab
Richard Torruellas <richard.torruellas@am.sony.com>
2017-02-08 15:57:14 -0800

    update deps

--- src/reducer.js
+++ src/reducer.js
@@ -5,7 +5,7 @@ export function gclient(state = immutableInitialState, action) {
   switch (action.type) {
     case '@@GQ_CLIENT/QUERY/SUCCESS':
     case '@@GQ_CLIENT/QUERY/ERROR':
-      return state.mergeDeep(action.data);
+      return state.merge(action.data);
     default:
       return state;
   }

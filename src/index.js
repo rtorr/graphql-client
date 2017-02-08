4bb0f0406ec16fe3f05f235a504b3ac4ed5a79ef
Richard Torruellas <richard.torruellas@am.sony.com>
2017-02-08 12:11:01 -0800

    adding operations to request

--- src/index.js
+++ src/index.js
@@ -22,10 +22,15 @@ function batchRequest(item) {
   if (!f) {
     throw Error(NO_FETCH_ERROR);
   }
+  const operations = item.ast.definitions.map(i => i.operation);
   return fetch(url, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-    body: JSON.stringify({ query: item.print, operations: [], variables: item.options.variables })
+    body: JSON.stringify({
+      query: item.print,
+      operations: operations ? operations : [],
+      variables: item.options.variables
+    })
   })
     .then(response => {
       return response.json();
